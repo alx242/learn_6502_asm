e5f0ad1280857d122b3a44fcda50ddb6f8076be8
Alexander Schüssler <alex@tail-f.com>
2014-10-11 23:36:00 +0200

    More updated comments :)

--- background.asm
+++ background.asm
@@ -1,7 +1,7 @@
         processor 6502
         org $1000               ; 4096 :)
-loop:   ldx #$04                ; Put the 04 hex into a registry
-        stx $d021               ; Pull data from sta into d021 (center area)
-        inx
-        stx $d020               ; Pull data from sta into d020 (border area)
+loop:   ldx #$04                ; Put the 04 hex into x registry
+        stx $d021               ; Pull data from x into d021 (center area)
+        inx                     ; Inc data in x
+        stx $d020               ; Pull data from x into d020 (border area)
         jmp loop
--- flicker.asm
+++ flicker.asm
@@ -1,5 +1,5 @@
         processor 6502
         org $1000               ; 4096 as usual
-loop:   inc $d021               ; Central area
-        inc $d020               ; Border area
+loop:   inc $d021               ; Inc bit data at central area
+        inc $d020               ; Inc bit data at border area
         jmp loop                ; Do it again :)
